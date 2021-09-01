8fd312082e723a5af334d015d7a6bb78a7935167
deej073 <ss3@gmail.com>
2021-09-01 14:53:39 -0700

    proc, gitignore

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
 
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
