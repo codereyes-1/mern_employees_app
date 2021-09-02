6b34b2881fd0413bb1182e211e9d85e68a12a44b
deej073 <ss3@gmail.com>
2021-09-01 18:44:32 -0700

    postbuild script v.1.0

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
@@ -9,6 +10,15 @@ app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
 
+// server static in prod env
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 app.listen(port, () => {
   // perform a database connection when server starts
