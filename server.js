c3a6128caec2b6fe165a72e8060f33bea8c321fe
deej073 <ss3@gmail.com>
2021-09-02 15:23:57 -0700

    proc, gitignore

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: "server/routes/config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.static("public"))
 app.use(express.json());
-app.use(require("./routes/record"));
+app.use(require("server/routes/record"));
 // get driver connection
-const dbo = require("./db/conn");
+const dbo = require("server/db/conn");
 
 // server static in prod env
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('server/public/build'));
 
   app.get('*', (req,res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.js'))
