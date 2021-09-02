578735ac017cc8566863c7a11c45a15f6a746f4a
deej073 <ss3@gmail.com>
2021-09-01 22:44:37 -0700

    proc, gitignore

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
+app.use(express.static("public"))
 app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
@@ -16,7 +17,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req,res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'app.js'))
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.js'))
   });
 }
 
