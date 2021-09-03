4472b5b77d65c6f232b16c571632366f05a296c9
deej073 <ss3@gmail.com>
2021-09-02 19:24:13 -0700

    pkg, proc, req > /server

--- server/server.js
+++ server/server.js
@@ -2,22 +2,22 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const cors = require("cors");
-require("dotenv").config({ path: "server/routes/config.env" });
+require("dotenv").config({ path: "config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.static("public"))
 app.use(express.json());
-app.use(require("server/routes/record"));
+app.use(require("./routes/record"));
 // get driver connection
-const dbo = require("server/db/conn");
+const dbo = require("./db/conn");
 
 // server static in prod env
 if(process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('server/public/build'));
+//   Set static folder
+  app.use(express.static("public"));
 
   app.get('*', (req,res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.js'))
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'server'))
   });
 }
 
