c2edc70679ec5e3a2876511e2b1106a7bc1c3fa6
deej073 <ss3@gmail.com>
2021-09-03 16:29:54 -0700

    /server

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const dbo = require("./db/conn");
 // server static in prod env
 if(process.env.NODE_ENV === 'production') {
 //   Set static folder
-  app.use(express.static("build"));
+  app.use(express.static("static"));
 
   app.get('*', (req,res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
