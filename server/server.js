46b4063650d50e47d9d5f3c6e9e4018de0efb454
deej073 <ss3@gmail.com>
2021-09-02 20:02:51 -0700

    /server

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static("public"));
 
   app.get('*', (req,res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'server'))
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.js'))
   });
 }
 
