01080d5c111e89a18076af15af822755e0dbfe61
deej073 <ss3@gmail.com>
2021-09-03 19:06:03 -0700

    /server

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -34,7 +34,7 @@ export default class RecordList extends Component {
 //  // This method will get the data from the database.
   componentDidMount() {
     axios
-      .get("http://localhost:5000/record/")
+      .get("https://employee-app-mern.herokuapp.com/record/")
       .then((response) => {
         this.setState({ records: response.data });
       })
@@ -57,7 +57,7 @@ export default class RecordList extends Component {
 
 //  // This method will delete a record based on the method
   deleteRecord(id) {
-    axios.delete("http://localhost:5000/" + id).then((response) => {
+    axios.delete("https://employee-app-mern.herokuapp.com/" + id).then((response) => {
       console.log(response.data);
     });
 
